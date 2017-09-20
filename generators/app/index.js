d2ef05f304ddc3a5e001f3c131786db4a044b6f2
janne <janva415@student.liu.se>
2017-09-20 23:43:28 +0200

    A rudimentary outline ready

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,22 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
+  // Private helpers
+  _cp(src, dest) {
+    this.fs.copy(
+      this.templatePath(src),
+      this.destinationPath(dest)
+    );
+  }
+
+  _cpTpl(src, dest, props) {
+    this.fs.copyTpl(
+      this.templatePath(src),
+      this.destinationPath(dest),
+      props
+    );
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
@@ -15,6 +31,9 @@ module.exports = class extends Generator {
       name: 'someAnswer',
       message: 'Would you like to enable this option?',
       default: true
+    }, {
+      name: 'project-name',
+      message: 'project name '
     }];
 
     return this.prompt(prompts).then(props => {
@@ -24,10 +43,18 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    // Fixed parts
+    this._cp('package.json', 'package.json');
+    this._cp('jsconfig.json', 'jsconfig.json');
+    this._cp('.eslintrc.js', '.eslintrc.js');
+    this._cp('.gitignore', '.gitignore');
+    // TODO: maybe  allow some options jquery, dash etc
+    this._cp('bower.json', 'bower.json');
+
+    // Flexible parts
+    this.fs.copyTpl('index.html', 'app/index.html', {
+      title: this.props.title
+    });
   }
 
   install() {
