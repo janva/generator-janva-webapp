1b0c48b4085afcec8bb5e3611e25c3905fe4dfba
janne <janva415@student.liu.se>
2017-09-25 11:21:15 +0200

    removed unused files

--- generators/app/templates/templateSpec.js
+++
@@ -1,8 +0,0 @@
-describe('temple engine', function () {
-
-    describe('fun', function () {
-        it('should return 3 ', function (done) {
-            excpect(fun().toBe(4));
-        });
-    });
-});
