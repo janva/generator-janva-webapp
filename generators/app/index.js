7cc818fdab7b54fc0bd2e9b1021c1962c50b372c
janne <janva415@student.liu.se>
2017-09-27 16:06:46 +0200

    renamed file and added editorconfg to work around issue with between linter and beutifier

--- generators/app/index.js
+++ generators/app/index.js
@@ -44,8 +44,10 @@ module.exports = class extends Generator {
     this._cp('gulpfile.js', 'gulpfile.js');
     this._cp('package.json', 'package.json');
     this._cp('jsconfig.json', 'jsconfig.json');
-    this._cp('.eslintrc.js', '.eslintrc.js');
+    this._cp('_eslintrc.js', '.eslintrc.js');
     this._cp('_gitignore', '.gitignore');
+    this._cp('_editorconfig', '.editorconfig');
+
     this._cp('main.less', 'app/styles/main.less');
     this._cp('main.js', 'app/scripts/main.js');
     // TODO: maybe  allow some options jquery, dash etc
